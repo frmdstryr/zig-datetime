b570d61187a64a32067720aa0c2385061685428a
Jimmi Holst Christensen <jhc@dismail.de>
2023-03-08 17:23:41 +0100

    Add as a module in build.zig

--- build.zig
+++ build.zig
@@ -15,6 +15,8 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("zig-datetime", .{ .source_file = .{ .path = "src/main.zig" } });
+
     const lib = b.addStaticLibrary(.{
         .name = "zig-datetime",
         // In this case the main source file is merely a path, however, in more
